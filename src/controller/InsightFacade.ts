a6654ed9a7dbfa9fc116c7678f36730b268b0e74
daphss <daphss@students.cs.ubc.ca>
2021-03-02 01:55:49 -0800

    lint fix

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -22,6 +22,97 @@ export default class InsightFacade implements IInsightFacade {
     constructor() {
         Log.trace("InsightFacadeImpl::init()");
     }
+
+    public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
+        let zip = new JSZip();
+        let courseOut: any[] = [];
+        if (kind !== InsightDatasetKind.Courses) {
+            return Promise.reject(new InsightError());
+        }
+        // let fs = require("fs");
+        let files = fs.readdirSync("./data");
+        if (files.includes(id)) {
+            return Promise.reject(new InsightError());
+        }
+        if (!this.idValidation(id)) {
+            return Promise.reject(new InsightError());
+        }
+        return new Promise((resolve, reject) => {
+            zip.loadAsync(content, {base64: true}).then(() => {
+                // if (zip.folder(/courses/).length === 0) {
+                //     reject(new InsightError());
+                // }
+                let list: any[] = [];
+                zip.folder("courses").forEach((relativepath: string, file: any) => {
+                    let inCourse = file.async("string");
+                    list.push(inCourse);
+                });
+                Promise.all(list).then((data: any) => {
+                    if (list.length <= 0) {
+                        reject(new InsightError());
+                    }
+                    this.parse(list, data, courseOut, id);
+                    if (courseOut.length <= 0) {
+                        reject(new InsightError());
+                    }
+                    let output = JSON.stringify(courseOut);
+                    let path = "./data/" + id;
+                    fs.writeFileSync(path, output);
+                    files.push(id);
+                    resolve(files);
+                });
+            });
+        });
+        // return Promise.reject("Not implemented.");
+    }
+
+    public removeDataset(id: string): Promise<string> {
+        if (!this.idValidation(id)) {
+            return Promise.reject(new InsightError());
+        }
+        // let fs = require("fs");
+        let files = fs.readdirSync("./data");
+        if (!files.includes(id)) {
+            return Promise.reject(new NotFoundError());
+        }
+        let path = "./data/" + id;
+        try {
+            fs.unlinkSync(path);
+        } catch (err) {
+            // Log.error(err);
+            return Promise.reject(new InsightError());
+        }
+        return Promise.resolve(id);
+    }
+
+    public performQuery(query: any): Promise<any[]> {
+        if (!Validity.isValid(query)) {
+            return Promise.reject(new InsightError());
+        }
+        return ExecuteQ.execute(query);
+    }
+
+    public listDatasets(): Promise<InsightDataset[]> {
+        // let fs = require("fs");
+        let files = fs.readdirSync("./data");
+        let list: any[] = [];
+        let out: InsightDataset[] = [];
+        return new Promise((resolve, reject) => {
+            for (const each of files) {
+                let path = "./data/" + each;
+                let infs = fs.readFileSync(path, "utf-8");
+                let temp = JSON.parse(infs.toString());
+                list.push(temp);
+                let insightOut: InsightDataset = {id: "", numRows: 0, kind: InsightDatasetKind.Courses};
+                insightOut.id = each;
+                insightOut.numRows = temp.length;
+                out.push(insightOut);
+            }
+            resolve(out);
+        });
+        // return Promise.reject("Not implemented.");
+    }
+
     private idValidation(id: string): boolean {
         if (id === undefined) {
             return false;
@@ -72,49 +163,6 @@ export default class InsightFacade implements IInsightFacade {
         return out;
     }
 
-    public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-        let zip = new JSZip();
-        let courseOut: any[] = [];
-        if (kind !== InsightDatasetKind.Courses) {
-            return Promise.reject(new InsightError());
-        }
-        // let fs = require("fs");
-        let files = fs.readdirSync("./data");
-        if (files.includes(id)) {
-            return Promise.reject(new InsightError());
-        }
-        if (!this.idValidation(id)) {
-            return Promise.reject(new InsightError());
-        }
-        return new Promise((resolve, reject) => {
-            zip.loadAsync(content, {base64: true}).then(() => {
-                // if (zip.folder(/courses/).length === 0) {
-                //     reject(new InsightError());
-                // }
-                let list: any[] = [];
-                zip.folder("courses").forEach((relativepath: string, file: any) => {
-                    let inCourse = file.async("string");
-                    list.push(inCourse);
-                });
-                Promise.all(list).then((data: any) => {
-                    if (list.length <= 0) {
-                        reject(new InsightError());
-                    }
-                    this.parse(list, data, courseOut, id);
-                    if (courseOut.length <= 0) {
-                        reject(new InsightError());
-                    }
-                    let output = JSON.stringify(courseOut);
-                    let path = "./data/" + id;
-                    fs.writeFileSync(path, output);
-                    files.push(id);
-                    resolve(files);
-                });
-            });
-        });
-        // return Promise.reject("Not implemented.");
-    }
-
     private parse(list: any[], data: any, courseOut: any[], id: string) {
         for (let i = 0; i < list.length; i++) {
             let currCourse;
@@ -131,52 +179,5 @@ export default class InsightFacade implements IInsightFacade {
                 courseOut.push(this.createCourse(id, each));
             }
         }
-    }
-
-    public removeDataset(id: string): Promise<string> {
-        if (!this.idValidation(id)) {
-            return Promise.reject(new InsightError());
-        }
-        // let fs = require("fs");
-        let files = fs.readdirSync("./data");
-        if (!files.includes(id)) {
-            return Promise.reject(new NotFoundError());
-        }
-        let path = "./data/" + id;
-        try {
-            fs.unlinkSync(path);
-        } catch (err) {
-            // Log.error(err);
-            return Promise.reject(new InsightError());
-        }
-        return Promise.resolve(id);
-    }
-
-    public performQuery(query: any): Promise<any[]> {
-        if (!Validity.isValid(query)) {
-            return Promise.reject(new InsightError());
-        }
-        return ExecuteQ.execute(query);
-    }
-
-    public listDatasets(): Promise<InsightDataset[]> {
-        // let fs = require("fs");
-        let files = fs.readdirSync("./data");
-        let list: any[] = [];
-        let out: InsightDataset[] = [];
-        return new Promise((resolve, reject) => {
-            for (const each of files) {
-                let path = "./data/" + each;
-                let infs = fs.readFileSync(path, "utf-8");
-                let temp = JSON.parse(infs.toString());
-                list.push(temp);
-                let insightOut: InsightDataset = {id: "", numRows: 0, kind: InsightDatasetKind.Courses};
-                insightOut.id = each;
-                insightOut.numRows = temp.length;
-                out.push(insightOut);
-            }
-            resolve(out);
-        });
-        // return Promise.reject("Not implemented.");
     }
 }
