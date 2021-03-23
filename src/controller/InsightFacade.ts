7487a79b6b323c13727e7f16cd9c5721512e4ef1
fongyuan <fong-yuan@hotmail.com>
2021-03-23 13:41:46 -0700

    refactored add courses to separate file and added room functionality

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -10,6 +10,8 @@ import * as JSZip from "jszip";
 import * as fs from "fs-extra";
 import Validity from "./Validity";
 import ExecuteQ from "./ExecuteQ";
+import AddCourse from "./AddCourse";
+import AddRoom from "./AddRoom";
 /**
  * This is the main programmatic entry point for the project.
  * Method documentation is in IInsightFacade
@@ -25,10 +27,10 @@ export default class InsightFacade implements IInsightFacade {
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
         let zip = new JSZip();
         let courseOut: any[] = [];
-        if (kind !== InsightDatasetKind.Courses) {
+        let roomOut: any[] = [];
+        if (!this.checkKind(kind)) {
             return Promise.reject(new InsightError());
         }
-        // let fs = require("fs");
         let files = fs.readdirSync("./data");
         if (files.includes(id)) {
             return Promise.reject(new InsightError());
@@ -36,33 +38,18 @@ export default class InsightFacade implements IInsightFacade {
         if (!this.idValidation(id)) {
             return Promise.reject(new InsightError());
         }
-        return new Promise((resolve, reject) => {
-            zip.loadAsync(content, {base64: true}).then(() => {
-                // if (zip.folder(/courses/).length === 0) {
-                //     reject(new InsightError());
-                // }
-                let list: any[] = [];
-                zip.folder("courses").forEach((relativepath: string, file: any) => {
-                    let inCourse = file.async("string");
-                    list.push(inCourse);
-                });
-                Promise.all(list).then((data: any) => {
-                    if (list.length <= 0) {
-                        reject(new InsightError());
-                    }
-                    this.parse(list, data, courseOut, id);
-                    if (courseOut.length <= 0) {
-                        reject(new InsightError());
-                    }
-                    let output = JSON.stringify(courseOut);
-                    let path = "./data/" + id;
-                    fs.writeFileSync(path, output);
-                    files.push(id);
-                    resolve(files);
-                });
-            });
-        });
-        // return Promise.reject("Not implemented.");
+        if (kind === InsightDatasetKind.Courses) {
+            return AddCourse.courseAdd(zip, content, courseOut, id, files);
+        } else {
+            return AddRoom.roomAdd(zip, content, roomOut, id, files);
+        }
+    }
+
+    private checkKind(kind: InsightDatasetKind) {
+        if (kind === InsightDatasetKind.Courses || kind === InsightDatasetKind.Rooms) {
+            return true;
+        }
+        return false;
     }
 
     public removeDataset(id: string): Promise<string> {
@@ -92,7 +79,6 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     public listDatasets(): Promise<InsightDataset[]> {
-        // let fs = require("fs");
         let files = fs.readdirSync("./data");
         let list: any[] = [];
         let out: InsightDataset[] = [];
@@ -109,7 +95,6 @@ export default class InsightFacade implements IInsightFacade {
             }
             resolve(out);
         });
-        // return Promise.reject("Not implemented.");
     }
 
     private idValidation(id: string): boolean {
@@ -129,54 +114,5 @@ export default class InsightFacade implements IInsightFacade {
             return false;
         }
         return true;
-    }
-
-    private createCourse(id: string, course: any): any {
-        let dept = id + "_dept";
-        let cid = id + "_id";
-        let avg = id + "_avg";
-        let instructor = id + "_instructor";
-        let title = id + "_title";
-        let pass = id + "_pass";
-        let fail = id + "_fail";
-        let audit = id + "_audit";
-        let uuid = id + "_uuid";
-        let year = id + "_year";
-        let section = id + "_section";
-        let out: any = {};
-        out[dept] = course["Subject"];
-        out[cid] = course["Course"];
-        out[avg] = course["Avg"];
-        out[instructor] = course["Professor"];
-        out[title] = course["Title"];
-        out[pass] = course["Pass"];
-        out[fail] = course["Fail"];
-        out[audit] = course["Audit"];
-        out[uuid] = course["id"];
-        if (course["Section"] === "overall") {
-            out[year] = 1990;
-        } else {
-            out[year] = course["Year"];
-        }
-        out[section] = course["Section"];
-        return out;
-    }
-
-    private parse(list: any[], data: any, courseOut: any[], id: string) {
-        for (let i = 0; i < list.length; i++) {
-            let currCourse;
-            try {
-                currCourse = JSON.parse(data[i]);
-            } catch (e) {
-                continue;
-            }
-            let sections = currCourse["result"];
-            if (sections.length <= 0) {
-                continue;
-            }
-            for (const each of sections) {
-                courseOut.push(this.createCourse(id, each));
-            }
-        }
     }
 }
